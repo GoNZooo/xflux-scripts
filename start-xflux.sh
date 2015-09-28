6ae68a7d2fad5045cbf15c930332470756b92913
Rickard Andersson <gonz@severnatazvezda.com>
2015-09-29 00:33:25 +0300

    Add location reading from file

--- start-xflux.sh
+++ start-xflux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 XFLUXPID=`ps ux | gawk -e '/^gonz.*xflux.*-l/ && !/grep/ && /-g/ { print $2 }'`
+LATITUDE=`gawk '/lat/ { print $2 }' location`
+LONGITUDE=`gawk '/long/ { print $2 }' location`
 
 if [[ -z $XFLUXPID ]]; then
-    xflux -l 42.6975100 -g 23.3241500
+    xflux -l $LATITUDE -g $LONGITUDE
 else
     echo "xflux already running at PID $XFLUXPID"
 fi
