104e76ae00ec8a2a7b3ae0b5a68edfc24cefbd8a
Rickard Andersson <gonz@severnatazvezda.com>
2015-10-06 02:07:03 +0300

    Add XFLUXSCRIPTS dir for location file

--- xflux-start.sh
+++ xflux-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 XFLUXPID=`ps ux | gawk -e '/^gonz.*xflux.*-l/ && !/grep/ && /-g/ { print $2 }'`
-LATITUDE=`gawk '/lat/ { print $2 }' location`
-LONGITUDE=`gawk '/long/ { print $2 }' location`
+LATITUDE=`gawk '/lat/ { print $2 }' "$XFLUXSCRIPTS/location"`
+LONGITUDE=`gawk '/long/ { print $2 }' "$XFLUXSCRIPTS/location"`
 
 if [[ -z $XFLUXPID ]]; then
     xflux -l $LATITUDE -g $LONGITUDE
